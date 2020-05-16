008ad1f5f734d62fed70ecbd6661066e0b8d4565
shh8228 <62638340+shh8228@users.noreply.github.com>
2020-05-17 01:07:17 +0900

    Add files via upload
    completed train file, network

--- network.py
+++ network.py
@@ -1,23 +1,91 @@
-
-import argparse
+# Referring to https://github.com/Tobias-Fischer/rt_gene/blob/master/rt_gene/src/rt_gene/gaze_estimation_models_pytorch.py
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
 
 
-class Net(nn.Module):
+class GazeEstimationAbstractModel(nn.Module):
 
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.mp = nn.MaxPool2d(2)
-        self.fc = nn.Linear(320, 10)
+        super(GazeEstimationAbstractModel, self).__init__()
 
-    def forward(self, data_h, data_l, data_r):
-        in_size = x.size(0)
-        x = F.relu(self.mp(self.conv1(x)))
-        x = F.relu(self.mp(self.conv2(x)))
-        x = x.view(in_size, -1)  # flatten the tensor
-        x = self.fc(x)
-        return F.log_softmax(x)
+    @staticmethod
+    def _create_fc_layers(in_features, out_features):
+        x_l = nn.Sequential(
+            nn.Linear(in_features, 1024),
+            nn.BatchNorm1d(1024, momentum=0.99, eps=1e-3),
+            nn.ReLU(inplace=True)
+        )
+        x_r = nn.Sequential(
+            nn.Linear(in_features, 1024),
+            nn.BatchNorm1d(1024, momentum=0.99, eps=1e-3),
+            nn.ReLU(inplace=True)
+        )
+
+        concat = nn.Sequential(
+            nn.Linear(2048, 512),
+            nn.BatchNorm1d(512, momentum=0.99, eps=1e-3),
+            nn.ReLU(inplace=True)
+        )
+
+        fc = nn.Sequential(
+            nn.Linear(514, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, out_features)
+        )
+
+        return x_l, x_r, concat, fc
+
+    def forward(self, left_eye, right_eye, headpose):
+        left_x = self.left_features(left_eye)
+        left_x = torch.flatten(left_x, 1)
+        left_x = self.xl(left_x)
+
+        right_x = self.right_features(right_eye)
+        right_x = torch.flatten(right_x, 1)
+        right_x = self.xr(right_x)
+
+        eyes_x = torch.cat((left_x, right_x), dim=1)
+        eyes_x = self.concat(eyes_x)
+
+        eyes_headpose = torch.cat((eyes_x, headpose), dim=1)
+
+        fc_output = self.fc(eyes_headpose)
+
+        return fc_output
+
+    @staticmethod
+    def _init_weights(modules):
+        for m in modules:
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight, mode="fan_in", nonlinearity="relu")
+                nn.init.zeros_(m.bias)
+
+
+class GazeEstimationModelVGG(GazeEstimationAbstractModel):
+
+    def __init__(self, num_out=2):
+        super(GazeEstimationModelVGG, self).__init__()
+        _left_model = models.vgg16(pretrained=True)
+        _right_model = models.vgg16(pretrained=True)
+
+        # remove the last ConvBRelu layer
+        _left_modules = [module for module in _left_model.features]
+        _left_modules.append(_left_model.avgpool)
+        self.left_features = nn.Sequential(*_left_modules)
+
+        _right_modules = [module for module in _right_model.features]
+        _right_modules.append(_right_model.avgpool)
+        self.right_features = nn.Sequential(*_right_modules)
+
+        for param in self.left_features.parameters():
+            param.requires_grad = True
+        for param in self.right_features.parameters():
+            param.requires_grad = True
+
+        self.xl, self.xr, self.concat, self.fc = GazeEstimationAbstractModel._create_fc_layers(
+            in_features=_left_model.classifier[0].in_features,
+            out_features=num_out)
+
+        GazeEstimationAbstractModel._init_weights(self.modules())
--- train.py
+++ train.py
@@ -1,108 +1,114 @@
 from __future__ import print_function
-import argparse
+
+import os
+
+import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
+from torch import from_numpy
 from torch.autograd import Variable
-from torch.utils.data import Dataset, DataLoader
-from torch import from_numpy, tensor
-import numpy as np
-from network import Net
-import os
-'''
-pytorch
-opencv
-'''
+from torch.utils.data import TensorDataset, DataLoader
+
+from network import GazeEstimationAbstractModel, GazeEstimationModelVGG
 
 # Training settings
-batch_size = 64
+batch_size = 1024
 
-class DiabetesDataset(Dataset):
+model = GazeEstimationModelVGG(GazeEstimationAbstractModel).cuda()
 
-    def __init__(self):
+optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.95))
+train_loss = nn.MSELoss()
+validation_loss = nn.MSELoss(reduction='sum')
+test_loss = nn.MSELoss(reduction='sum')
 
-    def __getitem__(self, index):
-        return self.x_data[index], self.y_data[index]
-
-    def __len__(self):
-        return self.len
-
-    def __load__(self, string, x, y):
-        root = os.path.join('./dataset', string, string)
-        self.x_data = from_numpy(np.load(os.path.join(root, x + '.npy')))
-        self.y_data = from_numpy(np.load(os.path.join(root, y + '.npy')))
-        self.len = self.x_data.shape[0]
-
-
-
-
-
-model = Net()
-
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
-
-
-def train(epoch):
-    #3-fold training set
-    for i in range(0, 17):
-        sess_str = 's' + str(i).zfill(3) + '_glasses'
-        dataset_left = DiabetesDataset()
-        dataset_left.__load__(sess_str, 'left_x', 'y_val')
-        dataset_right = DiabetesDataset()
-        dataset_right.__load__(sess_str, 'right_x','y_val')
-        dataset_head = DiabetesDataset()
-        dataset_head.__load__(sess_str, 'headPose_x', 'y_val')
-        #Cores checking : num_workers, batch size
-        train_loader_left = DataLoader(dataset=dataset_left,
-                                  batch_size=32,
-                                  shuffle=False,
-                                  num_workers=2)
-        train_loader_right = DataLoader(dataset=dataset_right,
-                                  batch_size=32,
-                                  shuffle=False,
-                                  num_workers=2)
-        train_loader_head = DataLoader(dataset=dataset_head,
-                                  batch_size=32,
-                                  shuffle=False,
-                                  num_workers=2)
-        model.train()
-        for batch_idx, (data_h, target) in enumerate(train_loader_head):
-            data_h, target = Variable(data_h), Variable(target)
-            data_l = Variable(train_loader_left[batch_idx][0])
-            data_r = Variable(train_loader_right[batch_idx][0])
-
-            optimizer.zero_grad()
-            output = model(data_h, data_l, data_r)
-            loss = F.nll_loss(output, target)
-            loss.backward()
-            optimizer.step()
-            if batch_idx % 10 == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data_r), len(train_loader_right.dataset),
-                    100. * batch_idx / len(train_loader_right), loss.item()))
-
-
-def test():
-    model.eval()
-    test_loss = 0
-    correct = 0
-    for data, target in test_loader:
-        data, target = Variable(data, volatile=True), Variable(target)
-        output = model(data)
-        # sum up batch loss
-        test_loss += F.nll_loss(output, target, size_average=False).data
-        # get the index of the max log-probability
-        pred = output.data.max(1, keepdim=True)[1]
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-
-    test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-
+# 3-fold
+training_set = [[1, 2, 8, 10], [3, 4, 7, 9]]
+test_set = [5, 6, 11, 12, 13]
+validation_set = [14, 15, 16]
 
 for epoch in range(1, 10):
-    train(epoch)
-    test()
+    for s in training_set:
+        for i in s:
+            sess_str = 's' + str(i).zfill(3) + '_glasses'
+
+            root = os.path.join('./dataset', sess_str, sess_str)
+            dataset = TensorDataset(from_numpy(np.load(os.path.join(root, 'left_x.npy'))).cuda(),
+                                    from_numpy(np.load(os.path.join(root, 'right_x.npy'))).cuda(),
+                                    from_numpy(np.load(os.path.join(root, 'headPose_x.npy'))).cuda(),
+                                    from_numpy(np.load(os.path.join(root, 'y_val.npy'))).cuda())
+
+            # Cores checking : num_workers, batch size
+            train_loader = DataLoader(dataset=dataset,
+                                      batch_size=32,
+                                      shuffle=False,
+                                      num_workers=2)
+
+            model.train()
+            for batch_idx, (data_l, data_r, data_h, target) in enumerate(train_loader):
+                data_l, data_r, data_h, target = Variable(data_l), Variable(data_r), Variable(data_h), Variable(target)
+                optimizer.zero_grad()
+                output = model(data_l, data_r, data_h)
+                loss = train_loss(output, target)
+                loss.backward()
+                optimizer.step()
+                if batch_idx % 10 == 0:
+                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        epoch, batch_idx * len(data_r), len(train_loader.dataset),
+                        100. * batch_idx / len(train_loader), loss.item()))
+
+        # Validation
+        for i in validation_set:
+            sess_str = 's' + str(i).zfill(3) + '_glasses'
+
+            root = os.path.join('./dataset', sess_str, sess_str)
+            dataset = TensorDataset(from_numpy(np.load(os.path.join(root, 'left_x.npy'))).cuda(),
+                                    from_numpy(np.load(os.path.join(root, 'right_x.npy'))).cuda(),
+                                    from_numpy(np.load(os.path.join(root, 'headPose_x.npy'))).cuda(),
+                                    from_numpy(np.load(os.path.join(root, 'y_val.npy'))).cuda())
+
+            # Cores checking : num_workers, batch size
+            validation_loader = DataLoader(dataset=dataset,
+                                           batch_size=32,
+                                           shuffle=False,
+                                           num_workers=2)
+
+            model.eval()
+            loss = 0
+            with torch.no_grad():
+                for batch_idx, (data_l, data_r, data_h, target) in enumerate(validation_loader):
+                    data_l, data_r, data_h, target = Variable(data_l), Variable(data_r), Variable(data_h), Variable(
+                        target)
+                    output = model(data_l, data_r, data_h)
+                    loss += validation_loss(output, target).data
+                loss /= len(validation_loader.dataset)
+                print('\nValidation set: Average loss: {:.4f}\n'.format(
+                    loss))
+
+    # Test
+    for i in test_set:
+        sess_str = 's' + str(i).zfill(3) + '_glasses'
+
+        root = os.path.join('./dataset', sess_str, sess_str)
+        dataset = TensorDataset(from_numpy(np.load(os.path.join(root, 'left_x.npy'))).cuda(),
+                                from_numpy(np.load(os.path.join(root, 'right_x.npy'))).cuda(),
+                                from_numpy(np.load(os.path.join(root, 'headPose_x.npy'))).cuda(),
+                                from_numpy(np.load(os.path.join(root, 'y_val.npy'))).cuda())
+
+        # Cores checking : num_workers, batch size
+        test_loader = DataLoader(dataset=dataset,
+                                 batch_size=32,
+                                 shuffle=False,
+                                 num_workers=2)
+
+        model.eval()
+        loss = 0
+        with torch.no_grad():
+            for batch_idx, (data_l, data_r, data_h, target) in enumerate(test_loader):
+                data_l, data_r, data_h, target = Variable(data_l), Variable(data_r), Variable(data_h), Variable(
+                    target)
+                output = model(data_l, data_r, data_h)
+                loss += test_loss(output, target).data
+            loss /= len(test_loader.dataset)
+            print('\nTest set: Average loss: {:.4f}\n'.format(
+                loss))
